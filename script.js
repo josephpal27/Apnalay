0527e7eb039c4529fecbcaabff8274d570cc683a
Joseph Pal <josephpal27@gmail.com>
2024-10-05 13:06:42 +0530

    done

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-document.getElementById('mySelect').addEventListener('change', function() {
-    if (this.value) {
-        this.style.color = '#000';  // Change color after selecting an option
-    } else {
-        this.style.color = '#bab9b9';  // Retain red color if no option is selected
-    }
-});
+

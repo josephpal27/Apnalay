c09d1132dc8abb80c049f7150f51ab259698f31d
Joseph Pal <josephpal27@gmail.com>
2024-10-15 13:51:35 +0530

    supporters page done

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,3 +17,43 @@ window.addEventListener("scroll", () => {
     backToTopDiv.classList.remove("show");
   }
 });
+
+
+// -------------------------------------------------------------------------------------------------------------------------------------
+
+// Functionality For Supporters Card Hover
+let supportersCards = document.querySelectorAll('.supporter-card');
+
+supportersCards.forEach((box) => {
+  box.addEventListener('mouseenter', () => {
+    box.style.backgroundColor = '#d25826';
+    box.style.transition = '0.8s ease';
+
+    // Get the h2 and p inside the specific card
+    let supportersCardH2 = box.querySelector('h2');
+    let supportersCardP = box.querySelectorAll('p');
+    
+    supportersCardH2.style.color = '#fff';
+    supportersCardH2.style.transition = '0.6s ease';
+
+    supportersCardP.forEach((cardP) => {
+      cardP.style.color = '#fff';
+      cardP.style.transition = '0.6s ease';
+    })
+  });
+
+  box.addEventListener('mouseleave', () => {
+    box.style.backgroundColor = ''; // Reset background color
+
+    // Reset the text color
+    let supportersCardH2 = box.querySelector('h2');
+    let supportersCardP = box.querySelectorAll('p');
+    
+    supportersCardH2.style.color = '';
+    supportersCardP.forEach((cardP) => {
+      cardP.style.color = '';
+    })
+  });
+});
+
+

40b6e317323c9900f41feac5d7217ab669151361
Joseph Pal <josephpal27@gmail.com>
2024-10-15 16:00:24 +0530

    done

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,12 +29,7 @@ supportersCards.forEach((box) => {
     box.style.backgroundColor = '#d25826';
     box.style.transition = '0.8s ease';
 
-    // Get the h2 and p inside the specific card
-    let supportersCardH2 = box.querySelector('h2');
     let supportersCardP = box.querySelectorAll('p');
-    
-    supportersCardH2.style.color = '#fff';
-    supportersCardH2.style.transition = '0.6s ease';
 
     supportersCardP.forEach((cardP) => {
       cardP.style.color = '#fff';
@@ -46,10 +41,8 @@ supportersCards.forEach((box) => {
     box.style.backgroundColor = ''; // Reset background color
 
     // Reset the text color
-    let supportersCardH2 = box.querySelector('h2');
     let supportersCardP = box.querySelectorAll('p');
     
-    supportersCardH2.style.color = '';
     supportersCardP.forEach((cardP) => {
       cardP.style.color = '';
     })

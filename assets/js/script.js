d4e864fbf4386a9ff083018ed473068e33fd7b0e
Joseph Pal <josephpal27@gmail.com>
2024-10-21 18:09:35 +0530

    done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,3 @@
-// Functionality For Banner
-var myCarousel = document.querySelector('#carouselExampleRide');
-var carousel = new bootstrap.Carousel(myCarousel, {
-    pause: false // Disable pause on hover
-});
-
-// -----------------------------------------------------------------------------------------------------------------------------------
 
 // Functionality For Back to Top Btn
 let backToTopDiv = document.querySelector(".back-to-top");
@@ -56,5 +49,62 @@ supportersCards.forEach((box) => {
     })
   });
 });
+
+
+// -------------------------------------------------------------------------------------------------------------------------------------
+
+// Functionality for Gallery Page Image Hover
+const galleryImgBoxes = document.querySelectorAll('.gallery-img');
+
+// Loop through each gallery image
+galleryImgBoxes.forEach(box => {
+    const layer = box.querySelector('.layer');
+
+    // On mouse enter, slide the layer up
+    box.addEventListener('mouseenter', () => {
+        layer.style.top = '0'; // Slide the layer to the top
+    });
+
+    // On mouse leave, slide the layer back down
+    box.addEventListener('mouseleave', () => {
+        layer.style.top = '100%'; // Reset the layer's position
+    });
+});
+
+
+
+// ------------------------------------------------------------------------------------------------------------------------------------
+
+let cards = document.querySelectorAll('.our-services .card-btn');
+let modal = document.querySelector('.modal');
+let closeBtn = document.querySelector('.fa-circle-xmark');
+
+// Add event listener to each partner
+cards.forEach(card => {
+    card.addEventListener('click', () => {
+        // Get the data attributes from the clicked partner
+        let title = card.getAttribute('data-title');
+        let desc = card.getAttribute('data-desc');
+        let image = card.getAttribute('data-image');
+
+        // Update the modal content
+        modal.querySelector('.modal-left h2').textContent = title;
+        modal.querySelector('.modal-left p').innerHTML = desc;
+        modal.querySelector('.modal-right img').src = image;
+
+        // Show the modal
+        modal.style.height = '100vh';
+    });
+});
+
+// Add event listener to the close button
+closeBtn.addEventListener('click', () => {
+    // Hide the modal
+    modal.style.height = '0';
+});
+
+
+
+// -------------------------------------------------------------------------------------------------------------------------------------
 
 
